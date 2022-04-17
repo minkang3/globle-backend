190dd942a54c68d776361e8cc5a73c2be06d920d
minkang543 <minkang.am@gmail.com>
2022-04-16 19:27:52 -0700

    added get players nearby and script to test get pi

---
+++ functions/get-players-nearby.js
@@ -0,0 +1,27 @@
+const calc_index = require("./calc-index.js");
+
+module.exports = get_players_nearby = (req) => {
+    const [latitude, longitude] = get_getaway(req);
+    /**
+     * assumption: 
+     * the getaways array is generated as 
+     * [ 1st lat long pair, 2nd lat long pair, ...
+     *   10th lat lon pair, ...
+     * ...,  100th lat lon, pair ]
+     *  where the first section is in the top left square then the next one is to the right, 
+     *  until finally the last one is bottom left.
+     * Generated like how english is read.
+     */
+
+     const index = calc_index( req.app.locals.MAX_LON, req.app.locals.MAX_LAT,
+                    req.app.locals.MIN_LON, req.app.locals.MIN_LAT,
+                    req.app.locals.DIVISION_FACTOR, req.body.longitude, 
+                    req.body.latitude );
+
+     return {
+         amount: req.app.locals.players_nearby[index],
+         index: index,
+         latitude: latitude,
+         longitude: longitude
+    };
+}
---
+++ scripts/test-picture.js
@@ -0,0 +1,3 @@
+const get_pic = require('../functions/get-pic.js');
+
+get_pic(33.8, -118.3);
